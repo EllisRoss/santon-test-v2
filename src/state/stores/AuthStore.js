bfcbbc183d3978cfb741412620901f04f3e1e935
EllisRoss <noumager@gmail.com>
2021-07-28 23:47:02 +0300

    add auth store

---
+++ src/state/stores/AuthStore.js
@@ -0,0 +1,31 @@
+import { Store } from "../common/store/store";
+import { Registry } from "../common/store/registry";
+
+export const SET_IS_AUTH = "SET_IS_AUTH";
+
+const AuthStore = new Store("auth", {
+  data: {
+    isAuth: false,
+  },
+  options: {
+    shouldInitFromState: true,
+    stateKey: "auth",
+  },
+  reducers: [
+    {
+      type: SET_IS_AUTH,
+      action(state, payload) {
+        const { isAuth } = payload;
+
+        return {
+          ...state,
+          isAuth,
+        };
+      },
+    },
+  ],
+});
+
+Registry.addStore(AuthStore);
+
+export { AuthStore };
